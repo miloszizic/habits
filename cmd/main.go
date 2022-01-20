11dab5845d984723837508461bf3d428e45c1d21
Milos Zizic <milos.zizic@sudstvo.me>
2022-01-20 16:45:58 +0100

    Template alert changes and refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -5,44 +5,36 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/miloszizic/habits/templates"
-
-	"github.com/miloszizic/habits/views"
-
-	"github.com/miloszizic/habits/controllers"
-
-	"github.com/miloszizic/habits/store"
-
 	"github.com/go-chi/chi/v5"
+	"github.com/miloszizic/habits/controllers"
+	"github.com/miloszizic/habits/store"
+	"github.com/miloszizic/habits/templates"
+	"github.com/miloszizic/habits/views"
 )
 
 func main() {
-	//habits.RunCLI()
+
 	store, err := store.FromSQLite("./habits.db")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "opening %q database: %v\n", err, store)
 		os.Exit(1)
 	}
 	r := chi.NewRouter()
-	handler := controllers.Handlers{Store: store}
+	srv := controllers.Server{Store: store}
 
-	handler.Templates.New, err = views.ParseFS(templates.Files, "home.gohtml", "tailwind.gohtml", "alerts.gohtml")
+	srv.Templates.New, err = views.ParseFS(templates.Files, "home.gohtml", "*.layout.gohtml")
 	if err != nil {
 		fmt.Errorf("error parsing %w", err)
 	}
-	r.Get("/", handler.Home)
-	handler.Templates.New, err = views.ParseFS(templates.Files, "habit.gohtml", "tailwind.gohtml", "alerts.gohtml")
+	r.Get("/", srv.Home)
+	srv.Templates.New, err = views.ParseFS(templates.Files, "habit.gohtml", "*.layout.gohtml")
 	if err != nil {
 		fmt.Errorf("error parsing %w", err)
 	}
-	r.Get("/habit", handler.Habit)
-
-	handler.Templates.New, err = views.ParseFS(templates.Files, "submit.gohtml", "tailwind.gohtml", "alerts.gohtml")
-	if err != nil {
-		fmt.Errorf("error parsing %w", err)
-	}
-	r.Post("/create", handler.Create)
+	r.Get("/habit", srv.Habit)
+	r.Post("/habit", srv.Create)
 
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
+
 }
--- controllers/server.go
+++ controllers/server.go
@@ -11,55 +11,55 @@ import (
 	"github.com/miloszizic/habits/store"
 )
 
-type Handlers struct {
+type Server struct {
 	Store     store.HabitStore
 	Templates struct {
 		New Template
 	}
 }
 
-func (h Handlers) Home(w http.ResponseWriter, r *http.Request) {
-	habits, err := h.Store.AllHabits()
+func (s Server) Home(w http.ResponseWriter, r *http.Request) {
+	habits, err := s.Store.AllHabits()
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	h.Templates.New.Execute(w, habits)
+	s.Templates.New.Execute(w, habits)
 
 }
 
-func (h Handlers) Habit(w http.ResponseWriter, r *http.Request) {
-	h.Templates.New.Execute(w, nil)
+func (s Server) Habit(w http.ResponseWriter, r *http.Request) {
+	s.Templates.New.Execute(w, nil)
 }
-func (h Handlers) Create(w http.ResponseWriter, r *http.Request) {
+func (s Server) Create(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	habitName := r.FormValue("name")
 	habit := store.Habit{Name: habitName}
-	exist, err := h.Store.GetHabit(habitName)
+	exist, err := s.Store.GetHabit(habitName)
 	if err != nil {
 		if err != sql.ErrNoRows {
 			vd.Alert = &views.Alert{
-				Level:   views.AlertLvlSuccess,
+				Color:   views.AlertLvlSuccess,
 				Message: fmt.Sprintf("You successfully created a %s Habit", habitName),
 			}
-			h.Store.Add(habit)
-			h.Templates.New.Execute(w, vd)
+			s.Store.Add(habit)
+			s.Templates.New.Execute(w, vd)
 			return
 		} else {
 			vd.Alert = &views.Alert{
-				Level:   views.AlertLvlError,
+				Color:   views.AlertLvlError,
 				Message: err.Error(),
 			}
-			h.Templates.New.Execute(w, vd)
+			s.Templates.New.Execute(w, vd)
 			return
 		}
 	}
 	if exist != nil {
 		vd.Alert = &views.Alert{
-			Level:   views.AlertLvlError,
+			Color:   views.AlertLvlError,
 			Message: "Habit already exists",
 		}
-		h.Templates.New.Execute(w, vd)
+		s.Templates.New.Execute(w, vd)
 	}
 }
